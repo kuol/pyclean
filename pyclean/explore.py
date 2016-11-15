75ae90fc99abfb0d7bc659773b57d5b01cd08ebc
kuol <liukuo99@gmail.com>
2016-11-15 13:43:29 -0700

    Add code to return columns with integer type that are possibility categorical

--- pyclean/explore.py
+++ pyclean/explore.py
@@ -15,11 +15,77 @@ def main():
     df_all = pd.read_csv(all_2007_2011, low_memory=False)
     df = variability_analysis(df_all)
 
-
+#==================================================
+# Which columns are: 
+#       - Numeric
+#       - String
+#==================================================
 def get_num_str_cols(df):
-    num_cols = [x for x in df.columns if df[x].dtype == np.float64 or df[x].dtype == np.int64]
+    float_cols = [x for x in df.columns if df[x].dtype == np.float64]
+    int_cols = [x for x in df.columns if df[x].dtype == np.int64]
+    num_cols = float_cols + int_cols
     cat_cols = [x for x in df if x not in num_cols]
-    return num_cols, cat_cols
+    return float_cols, int_cols, cat_cols
+
+#============================================================
+# Column type analysis: 
+#       - Is numeric really numeric?
+#             1) Float may be integer : 1.0, 2.0, 3.0 ...
+#             2) Integer may be categorical: 1,1,1,0,0,0,1
+#       - Is string really string?
+#             1) Percentage: 23%
+#             2) Currency:  $2323.23
+#             3) Comma seperated number: 1,234
+#             4) A combination of 2) and 3): $1,235.29
+#=============================================================
+def float_is_int(x):
+    if np.isnan(x):
+        return True
+    else:
+        return int(x) - x == 0
+
+def change_float_is_int(df, float_cols):
+    """ Change float column to int, if it essentially is integer type. """
+    for x in float_cols:
+        if all(df[x].apply(float_is_int)):
+            df[x] = df[x].apply(lambda x: int(x))
+
+
+def cols_int_is_categorical(df, int_cols, threshold = 5):
+    """ Return columns in integer type, but are possibilty categorical"""
+    threshold = min(threshold, df.shape[0]/10)
+    df_count = df[int_cols].apply(lambda x: len(set(x)))
+    return list(df_count[df_count < threshold].index)
+    
+
+def possible_nums(df, cat_cols, threshold = 5):
+    """ Return column names if column item are in the following format:
+        *) ' xx.x%   '
+        *) '  $xxx '
+        *) ' 12,347' -- TODO
+    """
+    threshold = min(threshold, df.shape[0]/10)
+    cols = []
+    for x in cat_cols:
+        n_percent_sign = sum(df[x].apply(lambda x: (str(x).strip()[-1] == '%')))
+        n_dollar_sign = sum(df[x].apply(lambda x: (str(x).strip()[0] == '$')))
+        n_comma_number = sum(df[x].apply(lambda x: bool(re.match("^[\d\,]*$", x))))
+        if n_percent_sign > threshold or n_dollar_sign > threshold or n_comma_number > threshold:
+            cols.append(x)
+    return cols 
+
+#============================================================
+# Column type fix: 
+#       - Is numeric really numeric?
+#             1) Float may be integer :  change float to integer
+#             2) Integer may be categorical: change integer to string
+#       - Is string really string?
+#             1) Percentage: 23%
+#             2) Currency:  $2323.23
+#             3) Comma seperated number: 1,234
+#             4) A combination of 2) and 3): $1,235.29
+#=============================================================
+
 
 def without_many_nans(df, threshold = 0.1):
     """ Return column names where the percent of not_nans is greater than threshold
@@ -63,37 +129,7 @@ def level_distribution(df, cat_cols, threshold = 0.1):
             cols.append(x)
     return cols, d
        
-def possible_nums(df, cat_cols, threshold = 5):
-    """ Return column names if column item are in the following format:
-        *) ' xx.x%   '
-        *) '  $xxx '
-        *) ' 12,347' -- TODO
-    """
-    threshold = min(threshold, df.shape[0]/10)
-    cols = []
-    for x in cat_cols:
-        n_percent_sign = sum(df[x].apply(lambda x: (str(x).strip()[-1] == '%')))
-        n_dollar_sign = sum(df[x].apply(lambda x: (str(x).strip()[0] == '$')))
-        n_comma_number = sum(df[x].apply(lambda x: bool(re.match("^[\d\,]*$", x))))
-        if n_percent_sign > threshold or n_dollar_sign > threshold or n_comma_number > threshold:
-            cols.append(x)
-    return cols
-
-
-def float_is_int(x):
-    if np.isnan(x):
-        return True
-    else:
-        return int(x) - x == 0
-
-def change_float_is_int(df, num_cols):
-    """ Change float column to int, if it essentially is integer type. """
-    for x in num_cols:
-        if df[x].dtype == np.float64:
-            if all(df[x].apply(float_is_int)):
-                df[x] = df[x].apply(lambda x: int(x))
-
-
+       
 def analyze_target(df, target, thres_level = 5):
     num_missing = df[target].isnull().sum()
     if num_missing > 0:
