b1e940052406a02139d9f1587db3dddf4c2c5dc5
Kuo Liu <liukuo99@gmail.com>
2016-11-17 15:02:21 -0700

    Add test for change_float_is_int, cols_int_is_categorical

--- test/test_diagnose_suggest.py
+++ test/test_diagnose_suggest.py
@@ -5,17 +5,25 @@ import pyclean.diagnose_suggest as ds
 
 class TestDiagnose(unittest.TestCase):
     def setUp(self):
-        d = {'a': ['we2', '%3a8', '23'],
-             'b': range(2,5),
-             'c': [1.2, 12, 34.0]}
+        d = {'a': ['we2', '%3a8', '23']*10,
+             'b': range(30),
+             'c': [1.2, 12, 34.0]*10,
+             'd': [1.0, 2.0]*15,}
         self.df = pd.DataFrame.from_dict(d)
-    def test_something(self):
+
+    def test_get_num_str_cols(self):
         float_cols, int_cols, cat_cols = ds.get_num_str_cols(self.df)
-        #print float_cols, int_cols, cat_cols
-        self.assertEqual(float_cols, ['c'])
+        self.assertEqual(float_cols, ['c', 'd'])
         self.assertEqual(int_cols, ['b'])
         self.assertEqual(cat_cols, ['a'])
+    
+    def test_change_float_is_int(self):
+        ds.change_float_is_int(self.df, ['c', 'd'])
+        self.assertEqual(self.df['d'].dtypes, 'int64')
 
+    def test_cols_int_is_categorical(self):
+        temp = ds.cols_int_is_categorical(self.df, ['b', 'd'])
+        self.assertEqual(temp, ['d'])
 
 
 
